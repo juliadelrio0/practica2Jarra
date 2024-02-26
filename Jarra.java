120a9b3dd93aec70b8ed5edef9e35934d6c13d81
Adriana Jubera <adrianajubera@MacBook-Pro-de-Adriana.local>
2024-02-26 16:34:27 +0100

    Añadir metodos llenaVacia

--- Jarra.java
+++ Jarra.java
@@ -15,16 +15,17 @@ public class Jarra {
 
 	// metodos
 	// 1
-	public int capacidad() {
-		return this.capacidad;
-	}
-	
-	public int cantidad() {
-		return this.cantidad;
+	// 2
+	public void llena() {
+    		this.cantidad = this.capacidad;
 	}
 
-	// 2
+	public void vacia() {
+   		this.cantidad = 0;
+	}
+
 	// 3
+
 
 	public void metodoComunitario(){
 	// Comentario incluido para pruebas
